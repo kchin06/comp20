017f2e5cc59971edd658a98c1c2437a4367b10ec
kchin06 <54953801+kchin06@users.noreply.github.com>
2020-04-23 21:54:55 -0400

    Create companies.js

---
+++ companies.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://kchin06:Christmas04040331@cluster0-snuu3.mongodb.net/test?retryWrites=true&w=majority";
+var fastcsv = require("fast-csv");
+var fs = require("fs");
+
+let readStream = fs.createReadStream("companies.csv");
+let companyData = [];
+let csvStream = fastcsv.parse().on("data", function(data) {
+    companyData.push({
+      company: data[0],
+      ticker: data[1]
+    });
+  })
+  .on("end", function() {
+    // remove the headings
+    companyData.shift();
+    console.log(companyData);
+
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+      (err, client) => {
+        if (err) throw err;
+
+      client.db("companies").collection("companies").insertMany(companyData, (err, res) => {
+        if (err) throw err;
+        console.log(`Data is inserted.`);
+      client.close();
+        });
+      }
+    );
+  });
+
+readStream.pipe(csvStream);
